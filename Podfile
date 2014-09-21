c5a73013a1152a65ab57181c13e3f50d6e796ca2
tmnb <tomonobu_sato@fablic.co.jp>
2014-09-21 11:34:24 +0900

    Added sample code

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '7.0'
+
+link_with 'SampleKit'
+pod 'AFNetworking'
---
+++ SampleKit/SampleKit.h
@@ -0,0 +1,19 @@
+//
+//  SampleKit.h
+//  SampleKit
+//
+//  Created by tomonobu on 2014/09/21.
+//  Copyright (c) 2014年 Tomonobu Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleKit.
+FOUNDATION_EXPORT double SampleKitVersionNumber;
+
+//! Project version string for SampleKit.
+FOUNDATION_EXPORT const unsigned char SampleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleKit/PublicHeader.h>
+
+#import <AFNetworking/AFNetworking.h>
